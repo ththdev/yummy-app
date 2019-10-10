d1f5c5580f6682532eefbeb7792807d6a3a4d7b4
ththdev <ththdev@gmail.com>
2019-10-10 12:32:18 +0900

    Add colors for global theming

---
+++ src/config/colors.tsx
@@ -0,0 +1,15 @@
+const colors = {
+  tint: '#417E81',
+  black: '#000000',
+  white: '#FFFFFF',
+  night: '#333333',
+  gray0: '#f2f2f2',
+  gray1: '#c4c4c4',
+  gray2: '#464848',
+  gradientTop: '#095050',
+  gradientBottom: '#417E81',
+  tabIconSelected: '#417E81',
+  tabIconDefault: '#f2f2f2',
+};
+
+export default colors;
