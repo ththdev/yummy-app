70b62daad148cfdd4e83e4bc18b8d92382558a42
ththdev <ththdev@gmail.com>
2019-10-14 11:39:35 +0900

    Implement Progressbar to register

--- src/components/auth/ProgressBar/index.tsx
+++ src/components/auth/ProgressBar/index.tsx
@@ -1,9 +1,11 @@
-import React, { useState, Dispatch, SetStateAction } from 'react'
-import { View, Animated, StyleSheet } from 'react-native'
+import React, { useState } from 'react'
+import { View, Animated, StyleSheet, Alert, Dimensions } from 'react-native'
 import { NavigationStackProp } from 'react-navigation-stack'
 import styled from 'styled-components/native'
 import Icon from 'react-native-vector-icons/Feather'
 import colors from '../../../config/colors'
+
+const { width } = Dimensions.get('window')
 
 const BackButton = styled.TouchableOpacity`
     width: 100%;
@@ -20,22 +22,50 @@ interface Props {
 
 const ProgressBar = ({navigation}: Props) => {
     const [ state, setState ] = useState({
-        percent: 25,
+        percent: new Animated.Value(1),
     })
     return (
         <View>
-            <BackButton onPress={() => setState({ percent: 50})}>
+            <BackButton onPress={
+                () => {
+                    Animated.timing(state.percent, {
+                        toValue: 2
+                    }).start()
+                }
+            }>
                 <Icon name="chevron-left" size={32} />
             </BackButton>
-            <Animated.View width={state.percent}/>
+            <Animated.View 
+                style={[
+                    { 
+                        width: state.percent.interpolate({
+                            inputRange: [1, 2, 3, 4],
+                            outputRange: [step(1), step(2), step(3), step(4)]
+                        }) 
+                    },
+                    styles.progressBar,
+                ]} 
+            />
         </View>
     )
 }
 
+const step = (step: number) => {
+    const depth = width / 4 * step
+    return depth
+}
+
+const increase = () => {
+    
+}
+
+const decrease = () => {
+    Alert.alert('decrease')
+}
+
 const styles = StyleSheet.create({
-    container: {
-        width: '25%',
-        height: 44,
+    progressBar: {
+        height: 1,
         backgroundColor: colors.tint
     }
 })
